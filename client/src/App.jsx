2df016ebf6546f139e24db4e27593032ea970ae8
Aditya Dhanraj <opsingh861@gmail.com>
2024-02-11 14:12:46 +0530

    output added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import Editor from './components/Editor';
+import Output from './components/Output';
 const App = () => {
   const [code, setCode] = useState('');
 
 
   return (
     <div className=''>
-
+      <Editor />
+      <Output />
     </div>
   );
 };
